96c9644cacd54746de23c4bfab210516a18a78e6
gyrusdentatus <33793809+gyrusdentatus@users.noreply.github.com>
2017-11-22 01:23:00 +0100

    Add files via upload

---
+++ Scraper.py
@@ -0,0 +1,37 @@
+import praw 
+import csv
+r = praw.Reddit(user_agent='Comment Extraction (by/u/crystalballroom)',
+                client_id='5Ofau0MfjYSsfw', client_secret="NFOZVdPeCNBgX6W7ThdrTUrGkpM", 
+                username='gyrusdentatus', password='ou3-ynd-6tn-Kcb')
+import logging
+
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger = logging.getLogger('prawcore')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+
+sub = r.subreddit('NEO')
+
+submission = r.submission(url="https://www.reddit.com/r/NEO/comments/7e8cz5/daily_discussion_november_20th_2017/")
+
+filename = "ua.csv"
+f = open(filename, "w")
+
+submission.comments.replace_more(limit=0)
+for top_level_comment in submission.comments:
+    f.write(top_level_comment.body)
+
+submission.comments.replace_more(limit=0)
+for top_level_comment in submission.comments:
+    for second_level_comment in top_level_comment.replies:
+        f.write(second_level_comment.body)
+
+submission.comments.replace_more(limit=0)
+for comment in submission.comments.list():
+    f.write(comment.body)    
+
+    f.write(top_level_comment + "," + product_name + "," + shipping + "\n")
+f.close()    
